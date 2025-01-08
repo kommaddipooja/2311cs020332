1a4ee8f1d49696b9463be4dfaa644b13c8661fb5
kommaddipooja <poojakommaddi@gmail.com>
2025-01-08 06:53:52 +0000

    any name about project

---
+++ day1.py
@@ -0,0 +1,9 @@
+integer_var = 42
+float_var = 3.14
+string_var = "Hello, Jupyter!"
+boolean_var = True
+
+print(f"Integer Variable: {integer_var}, Type: {type(integer_var)}")
+print(f"Float Variable: {float_var}, Type: {type(float_var)}")
+print(f"String Variable: {string_var}, Type: {type(string_var)}")
+print(f"Boolean Variable: {boolean_var}, Type: {type(boolean_var)}")
